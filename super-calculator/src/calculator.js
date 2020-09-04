947c6b2edcfd5060d5c49c2d9e1a9bc7453d7271
diegosalazar343 <diegosalazar343@gmail.com>
2020-09-04 16:46:40 -0700

    final commit will need to remake environment

--- super-calculator/src/calculator.js
+++ super-calculator/src/calculator.js
@@ -5,3 +5,5 @@ export default function Calculator(age){
 Calculator.prototype.checkType = function(){
   return "I dont know";
 };
+
+// Need 5 forms, one for each planet and one to determine life expectancy.(Maybe always make it zero since there is no oxygen on other planets).
