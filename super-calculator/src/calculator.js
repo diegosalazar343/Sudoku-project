f811290fd624a3fbc3d6976c7f52fbc1d4b8c95a
Diego Salazar <salazardiego343@gmail.com>
2020-11-11 09:17:05 -0800

    Delete calculator.js

--- super-calculator/src/calculator.js
+++
@@ -1,9 +0,0 @@
-export default function Calculator(age){
-  this.age = age;
-}
-
-Calculator.prototype.checkType = function(){
-  return "I dont know";
-};
-
-// Need 5 forms, one for each planet and one to determine life expectancy.(Maybe always make it zero since there is no oxygen on other planets).
